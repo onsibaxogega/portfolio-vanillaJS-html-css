fbc7151d6958b1fb53b39d36f8fd8808a77bfbb7
onsibaxogega <onsibaxogega@gmail.com>
2024-11-15 16:25:23 -0600

    about section
    about section progress

--- script.js
+++ script.js
@@ -5,11 +5,3 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const savedTheme = localStorage.getItem("theme");
-    const userPrefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
-    document.documentElement.setAttribute(
-        "data-theme",
-        savedTheme || (userPrefersLight ? "light" : "dark")
-    );
-});
