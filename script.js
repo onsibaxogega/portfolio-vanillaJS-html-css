9fbd19bffaf7148a3155b2be6ddadf81784c3709
onsibaxogega <onsibaxogega@gmail.com>
2024-12-08 05:03:41 -0600

    Created contact section and populated about

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+window.addEventListener('DOMContentLoaded', ()=> {
+    if (localStorage.getItem('theme') === 'light') document.documentElement.classList.add('lightMode');
+    else localStorage.setItem('theme', 'dark');
+});
+
+let toggleTheme = ()=> {
+    const currentTheme = localStorage.getItem('theme');
+
+    switch (currentTheme) {
+        case 'dark':
+            document.documentElement.classList.add('lightMode');
+            localStorage.setItem('theme', 'light');
+            break;
+        default:
+            document.documentElement.classList.remove('lightMode');
+            localStorage.setItem('theme', 'dark');
+            break;
+
+    }
+}
+
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-
